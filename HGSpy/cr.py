bcf04932df0a6afb35385b0e9c1895cd37a94b72
CalebFuster <calebfuji@gmail.com>
2024-05-07 20:14:37 +0200

    no message

--- HGSpy/cr.py
+++ HGSpy/cr.py
@@ -6,6 +6,7 @@
 #
 # Arnau Miro, Elena Terzic
 from __future__ import print_function, division
+import os
 
 import numpy as np, time as time_module, functools
 
@@ -231,9 +232,9 @@ def cr(ch_name,suff=0):
 	def decorator(func):
 		@functools.wraps(func)
 		def wrapper(*args,**kwargs):
-			cr_start(ch_name,suff)
+			if os.getenv("UNITTEST", 'True').lower() in ('true', '1', 't'): cr_start(ch_name, suff)
 			out = func(*args,**kwargs)
-			cr_stop(ch_name,suff)
+			if os.getenv("UNITTEST", 'True').lower() in ('true', '1', 't'): cr_stop(ch_name, suff)
 			return out
 		return wrapper
 	return decorator
