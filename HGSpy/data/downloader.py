293a975a0773d5b30241d4f88ba46f032104ffdf
CalebFuster <calebfuji@gmail.com>
2024-05-19 12:09:27 +0200

    Missing python2 __future__

--- HGSpy/data/downloader.py
+++ HGSpy/data/downloader.py
@@ -8,7 +8,6 @@ By Caleb Fuster, Manel Soria and Arnau Miró
 ESEIAAT UPC      
 ***********************************************************************************************************
 '''
-from __future__ import print_function, division
 
 import os, urllib.request, numpy as np
 
