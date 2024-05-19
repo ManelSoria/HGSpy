06b3b8f4055d6472987d5d0128c17734776e85ea
CalebFuster <calebfuji@gmail.com>
2024-05-19 11:36:02 +0200

    Black formatting code

--- HGSpy/hgs_print.py
+++ HGSpy/hgs_print.py
@@ -1,4 +1,4 @@
-'''
+"""
 ***********************************************************************************************************
 HGS CHEMICAL EQUATION SOLVER
 
@@ -7,11 +7,12 @@ HGS Mixture main functions
 By Caleb Fuster, Manel Soria and Arnau Miró
 ESEIAAT UPC      
 ***********************************************************************************************************
-'''
+"""
+
 from HGSpy.utils import raiseError
 
 
-def hgs_print_info(name,hgs_data):
+def hgs_print_info(name, hgs_data):
     """
     *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
 
@@ -48,19 +49,19 @@ def hgs_print_info(name,hgs_data):
         print(f"Species = <{hgs_data['name'][ids]}>   code = {ids}", end="\n")
         ena = hgs_data["ena"][ids]  # element names
         nat = hgs_data["nat"][ids]
-        ne  = len(ena)  # number of elements
+        ne = len(ena)  # number of elements
         print("- Composition: ", end=" ")
         for jj in range(ne):
             print(f"{ena[jj]}-{nat[jj]:.0f}  ", end=" ")
         print(f"\n- Mm = {hgs_data['mm'][ids]:.4f} ", end="\n")
-        print(' -----------------\n')
+        print(" -----------------\n")
     else:
         ids -= ns
         print(f'Combination = <{hgs_data["comb"][ids]}>   code = {ids + ns}', end="\n")
-        cen = hgs_data["cspec"][ids] # combination element names
+        cen = hgs_data["cspec"][ids]  # combination element names
         cna = hgs_data["cper"][ids]  # combination element atoms
-        ne  = len(cna)               # number of  elements
-        print('- Composition  ', end=" ")
+        ne = len(cna)  # number of  elements
+        print("- Composition  ", end=" ")
         for jj in range(ne):
-            print(f'{cen[jj]}: { cna[jj]:.2f}%.  ', end=" ")
-        print('\n -----------------\n')
+            print(f"{cen[jj]}: { cna[jj]:.2f}%.  ", end=" ")
+        print("\n -----------------\n")
